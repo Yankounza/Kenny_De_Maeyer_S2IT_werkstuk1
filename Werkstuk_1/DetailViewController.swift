1b859d5715af79f0d788216d03ff784cdc284100
ontlener <ontlener@macbook-air-van-ontlener.local>
2018-05-04 15:31:09 +0200

    detail view part 2 with segue to image view

--- Werkstuk_1/DetailViewController.swift
+++ Werkstuk_1/DetailViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class DetailViewController: UIViewController {
 
@@ -20,6 +22,7 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var city: UILabel!
     @IBOutlet weak var country: UILabel!
     @IBOutlet weak var image: UIImageView!
+    @IBOutlet var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,16 +31,34 @@ class DetailViewController: UIViewController {
         self.firstname.text = item.firstName
         self.lastname.text = item.lastName
         self.street.text = item.address.street
-        self.number = item.address.number
-        self.zipcode.text = item.address.zipcode
+        self.number.text = String(item.address.number)
+        self.zipcode.text = String(item.address.zipcode)
         self.city.text = item.address.city
         self.country.text = item.address.country
-        self.image = UIImage(named: item.address.image)
+        self.image.image = UIImage(named: item.picture)
+        
+        let location = CLLocation(latitude: item.address.latitude, longitude: item.address.longitude)
+        let radius: CLLocationDistance = 1000
+        
+        func centerMapOnLocation(location: CLLocation) {
+            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, radius, radius)
+            mapView.setRegion(coordinateRegion, animated: true)
+        }
+        
+        centerMapOnLocation(location: location)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ImageView"
+        {
+            let vc = segue.destination as! ImageViewController
+            vc.image = self.item.picture
+        }
     }
     
 
---
+++ Werkstuk_1/ImageViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ImageViewController.swift
+//  Werkstuk_1
+//
+//  Created by ontlener on 04/05/2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    let image = ""
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        imageView.image = UIImage(named: image)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
